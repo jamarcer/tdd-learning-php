a2319289968f174859bc6277fec76a7f2243dd4b
Jesus Martinez <jamartinez@undanet.com>
2022-08-29 18:34:32 +0200

    🔴 Element at position 2 is 2

--- katas/fizz-buzz/tests/FizzBuzzTest.php
+++ katas/fizz-buzz/tests/FizzBuzzTest.php
@@ -9,9 +9,14 @@ use PHPUnit\Framework\TestCase;
 
 class FizzBuzzTest extends TestCase
 {
-    public function test_that_first_element_in_the_outout_is_1()
+    public function test_that_first_element_in_the_outout_is_1(): void
     {
         $this->assertEquals(1, $this->kata()->elementAtPosition(1));
+    }
+
+    public function test_that_second_element_in_the_outout_is_2(): void
+    {
+        $this->assertEquals(2, $this->kata()->elementAtPosition(2));
     }
 
     private function kata(): FizzBuzz
