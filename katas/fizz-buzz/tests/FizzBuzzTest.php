604cf4cffc4d13a65725a9cbeac0f1dbc0172ec9
Jesus Martinez <jamartinez@undanet.com>
2022-08-29 18:44:01 +0200

    🔴 Element at position 5 is Buzz

--- katas/fizz-buzz/tests/FizzBuzzTest.php
+++ katas/fizz-buzz/tests/FizzBuzzTest.php
@@ -9,19 +9,24 @@ use PHPUnit\Framework\TestCase;
 
 class FizzBuzzTest extends TestCase
 {
-    public function test_that_first_element_in_the_outout_is_1(): void
+    public function test_that_the_first_element_in_the_outout_is_1(): void
     {
         $this->assertEquals(1, $this->kata()->elementAtPosition(1));
     }
 
-    public function test_that_second_element_in_the_outout_is_2(): void
+    public function test_that_the_second_element_in_the_outout_is_2(): void
     {
         $this->assertEquals(2, $this->kata()->elementAtPosition(2));
     }
 
-    public function test_that_third_element_in_the_outout_is_fizz(): void
+    public function test_that_the_third_element_in_the_outout_is_fizz(): void
     {
         $this->assertEquals('Fizz', $this->kata()->elementAtPosition(3));
+    }
+
+    public function test_that_the_fifth_element_in_the_outout_is_buzz(): void
+    {
+        $this->assertEquals('Buzz', $this->kata()->elementAtPosition(5));
     }
 
     private function kata(): FizzBuzz
